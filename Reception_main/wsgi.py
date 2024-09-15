5adbd961bd481fce54bcb4253aa4c59742ac27a7
oscar <oscarmureti75@gmail.com>
2024-09-15 16:01:50 +0300

    rename

--- Reception_main/wsgi.py
+++ Reception_main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Receiption project.
+WSGI config for Reception_main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Receiption.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reception_main.settings')
 
 application = get_wsgi_application()
